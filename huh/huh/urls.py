91452d6f3d1dbe4d25af3d96d1eff323434537d1
Loba <iamtobi09@gmail.com>
2022-07-13 00:15:25 +0100

    lol

--- huh/huh/urls.py
+++ huh/huh/urls.py
@@ -1,4 +1,4 @@
-"""userapi URL Configuration
+"""huh URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- userapi/users/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Country(models.Model):
-    name = models.CharField(max_length=100)
-    capital = models.CharField(max_length=100)
-    area = models.IntegerField(help_text="(in square kilometers)")
